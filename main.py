ea175a14beb60859ab1015a97e674360ef4a5071
Dupaul Braedon <816947@stma.k12.mn.us>
2021-03-31 10:16:00 -0500

    Closer to 10 try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if abs(x) >= 10:
+        print(x)
+    
+    elif abs(y) >= 10:
+        print(y)
+    
+    else:
+        print(0)
+
+print(close10(15, 15))
